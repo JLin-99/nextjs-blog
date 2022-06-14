95b14825877dc62299f385c847cab205d7209ce0
José Lin <lin-jose@outlook.com>
2022-06-14 18:49:54 -0200

    Add Third-Party JavaScript with Script Component
    As `next/script` is an extension of the HTML `<script>` element
    that optimizes when additional scripts are fetched and executed.
    
    The Facebook SDK was only used as a contrived example to show how
    to add third-party scripts to the application in a performant way.

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Script from 'next/script';
 import Link from 'next/link';
 
 export default function FirstPost() {
@@ -7,6 +8,13 @@ export default function FirstPost() {
 			<Head>
 				<title>First Post</title>
 			</Head>
+			<Script
+				src="https://connect.facebook.net/en_US/sdk.js"
+				strategy="lazyOnload"
+				onLoad={() =>
+					console.log(`script loaded correctly, window.FB has been populated`)
+				}
+			/>
 			<h1>First Post</h1>
 			<h2>
 				<Link href="/">
